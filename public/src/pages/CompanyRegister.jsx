0241b0dd6eec4f3d60f36b73d1e7e1371e2d7b4d
Rohit Raj <rohit06092000@gmail.com>
2024-12-19 00:35:24 +0530

    Initial Project Setup

---
+++ public/src/pages/CompanyRegister.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CompanyRegister() {
+  return (
+    <div>CompanyRegister</div>
+  )
+}
+
+export default CompanyRegister
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require("dotenv").config()
+const userRoutes = require('./routes/userRoutes')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/auth", userRoutes)
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("MongoDB successfully connected")
+}).catch((err)=>{
+    console.log(err.message)
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server is hosted at http://localhost:8000")
+})
